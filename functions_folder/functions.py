8de1a01f45a89a62f9bb7c92adf80626f91ad644
cyn4 <cynthiakirabo1@gmail.com>
2024-01-03 19:44:37 +0300

    Update to functions

--- functions_folder/functions.py
+++ functions_folder/functions.py
@@ -263,7 +263,8 @@ def display_places(places):
                 print()
     else:
         print("Search not found")
-        import requests
+     
+     
 def get_location_coordinates(place):
     """
     Accesses the Google Maps API and returns the coordinates of a place.
@@ -303,32 +304,3 @@ def get_location_coordinates(place):
         return None
 
 
-def display_places(places):
-    """
-    Displays a list of places, ratings, and addresses based on the provided data.
-
-    Args:
-        places (list): List of places with details.
-
-    Returns:
-        None
-    """
-
-    if places:
-        print("Places:")
-
-        # Open a text file to store the search results
-        with open('user_search.txt', 'a') as text_file:  # 'a' mode for appending
-            for idx, place in enumerate(places, start=1):
-                # Write the place information to the text file
-                text_file.write(f"{idx}. {place['name']}\n")
-                text_file.write(f"Rating: {place.get('rating', 'Not rated')}\n")
-                text_file.write(f"Address: {place.get('vicinity', 'Address not available')}\n\n")
-
-                # Print the same content to the console
-                print(f"{idx}. {place['name']}")
-                print("Rating:", place.get('rating', 'Not rated'))
-                print("Address:", place.get('vicinity', 'Address not available'))
-                print()
-    else:
-        print("Search not found")
