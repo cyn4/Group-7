2e958adacc86476a4548e498da3ad78175f7362e
Zione Kamtambe <61716952+Zionie111@users.noreply.github.com>
2024-01-05 07:40:43 +0200

    Zione Kamtambe

--- functions_folder/clear_console.py
+++ functions_folder/clear_console.py
@@ -1,7 +1,6 @@
 def clear_console():
     """
     Clears console screen based on OS.
-
     This function uses the os.name attribute to check the user's operating system.
     If the operating system is Windows ('nt'), it uses the 'cls' command to clear
     the console.
