96ca1e7e01e5d1e3338b3866bc4b4075f7a6834a
Zione Kamtambe <61716952+Zionie111@users.noreply.github.com>
2024-01-05 15:50:46 +0200

    Zione Kamtambe

--- functions_folder/push_navigation_services.py
+++ functions_folder/push_navigation_services.py
@@ -1,10 +1,10 @@
-def check_navigation_services(): 
+def push_navigation_services(): 
     """
     Function to prompt the user about exploring the navigation service.
 
     Returns:
     - str: User's choice ('Yes' or 'No')
     """
-    navigation_check = input("Hey🙋🏻‍♀️! before you go, would you love to check out our Navigation "
+    navigation_push = input("Hey🙋🏻‍♀️! before you go, would you love to check out our Navigation "
                             "service.\n Type Yes or No: ").lower()
-    return navigation_check
+    return navigation_push
