80ab6fef365af48b1864b6020f3d0db046e41a38
Zione Kamtambe <61716952+Zionie111@users.noreply.github.com>
2024-01-05 07:30:09 +0200

    Zione Kamtambe

--- functions_folder/display_places.py
+++ functions_folder/display_places.py
@@ -3,24 +3,20 @@ def display_places(places):
     Displays a list of places, ratings, and addresses based on the provided data.
 
     Args:
-        places (list): List of places with details.
+    - places (list): List of places with details.
 
     Returns:
-        None
+    - None
     """
-
     if places:
         print("Places:")
-
-        # Open a text file to store the search results
-        with open('user_search.txt', 'a') as text_file:  # 'a' mode for appending
+        # Write place information to a text file for reference
+        with open('user_search.txt', 'w', encoding='utf-8') as text_file:
             for idx, place in enumerate(places, start=1):
-                # Write the place information to the text file
                 text_file.write(f"{idx}. {place['name']}\n")
                 text_file.write(f"Rating: {place.get('rating', 'Not rated')}\n")
                 text_file.write(f"Address: {place.get('vicinity', 'Address not available')}\n\n")
-
-                # Print the same content to the console
+                # Printing the same content to the console for immediate visibility
                 print(f"{idx}. {place['name']}")
                 print("Rating:", place.get('rating', 'Not rated'))
                 print("Address:", place.get('vicinity', 'Address not available'))
