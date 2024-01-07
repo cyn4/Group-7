3b19c19de540b7a0ec1785febc15f9a95042e19a
Oluwatobi Ojo <131699315+Teewalade@users.noreply.github.com>
2024-01-06 16:55:15 -0800

    Update functions.py

--- merge_folder/functions.py
+++ merge_folder/functions.py
@@ -371,15 +371,15 @@ def navigation_services():
             break
 
 
-# def app_exit(): 
-#     """
-#     Function to ask the user if they want to exit the app.
+ def app_exit(): 
+     """
+    Function to ask the user if they want to exit the app.
 
-#     Returns:
-#     - str: User's choice ('Yes' or 'No')
-#     """
-#     exit = input("Do you want to exit app. Type Yes or No:  ").lower()
-#     return exit
+     Returns:
+     - str: User's choice ('Yes' or 'No')
+     """
+     exit = input("Do you want to exit app. Type Yes or No:  ").lower()
+    return exit
 
 
 
