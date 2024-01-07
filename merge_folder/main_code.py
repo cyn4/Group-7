26b032c9d81dd9742cb9c3914eaf3d95c6bb1f8b
cyn4 <cynthiakirabo1@gmail.com>
2024-01-07 11:46:11 +0300

    Cynthia, copied main file

--- merge_folder/main_code.py
+++ merge_folder/main_code.py
@@ -1,29 +1,29 @@
-from functions import get_location_coordinates, display_places, get_nearby_places, get_user_input, weather_check, direction, \
-    clear_console
-from travel_guide_logo import logo, greeting  # Importing specific art elements for visual display
-if __name__ == "__main__":
+# Importing necessary functions from 'functions' module and specific art elements from 'Title' module
+from functions import discover_places_service, push_navigation_services, navigation_services, app_exit, clear_console
+from Title import logo # Importing specific art elements for visual display
+
+if __name__ == "__main__": # Main execution of the program
     usage = True
     while usage:
-        print(greeting, logo)  # Printing greeting and logo art
-        # Getting user input
-        user_city, user_country, search, user_rating, number_of_output = get_user_input()
-        place = user_city, user_country
-        location = get_location_coordinates(place)  # Getting coordinates for the user's location
-
-        if location:
-            # Displaying weather information for the user's location
-            print(weather_check(user_city, user_country))
-            # Getting and displaying nearby places based on user's input
-            nearby_places = get_nearby_places(location, search, user_rating, number_of_output)
-            display_places(nearby_places)
-
-        travel = input("🧭 Will you like navigation service\n Type Yes or No: ").lower()  # Asking if the user wants navigation service
-        if travel == 'yes':
-            direction()  # Providing navigation directions if requested
-
-        user_usage = input('Do you have other fun places and destinations to check out 😃?\nType yes or no: ').lower()
-        if user_usage == "no":
-            print("Sad to see you leave 😔. Check back soon.")
-            break
-        else:
-            clear_console()  # Clearing the console for the next iteration
+        print(logo) # Printing greeting and logo art
+        service_type = input(
+            "Do you want to Discover places 🏨 or Do you want Navigation services 🗺️\n Type (Places or "
+            "Navigation): ").lower()
+        if service_type == "places":
+            discover_places_service() # Executing function to discover places
+            if push_navigation_services() == "yes": # Asking user if they want to proceed with navigation services
+                navigation_services() # Executing function to provide navigation services
+            if app_exit() == "yes": # Asking user if they want to exit the program
+                print("Sad to see you leave 😔. Check back soon.")
+                break
+            else:
+                clear_console() # Clearing console for a fresh start
+        if service_type == "navigation":
+            navigation_services() # Executing function to provide navigation services
+            print("\n")
+            if app_exit() == "yes": # Asking user if they want to exit the program
+                print("Sad to see you leave 😔. Check back soon.")
+                print("\n")
+                break
+            else:
+                clear_console() # Clearing console for a fresh start
